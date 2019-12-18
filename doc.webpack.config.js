e394fcccbe01706eef066171679e7ef1d0057a02
Guo Yunhe <i@guoyunhe.me>
2019-12-18 11:48:47 +0200

    use typescript ts

--- doc.webpack.config.js
+++ doc.webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-  entry: "./doc/index.js",
+  entry: "./doc/index.ts",
   output: {
     path: path.resolve(__dirname, "doc/dist")
   },
